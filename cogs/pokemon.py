b7309499313938807a63fee03598a0670bda0b90
witherr <sdssouvic99@gmail.com>
2023-09-22 08:36:07 +0530

    Complete POK-88: Add --paradox flag

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -97,6 +97,7 @@ class Pokemon(commands.Cog):
     @flags.add_flag("--alolan", action="store_true")
     @flags.add_flag("--galarian", action="store_true")
     @flags.add_flag("--hisuian", action="store_true")
+    @flags.add_flag("--paradox", action="store_true")
     @flags.add_flag("--mythical", action="store_true")
     @flags.add_flag("--legendary", action="store_true")
     @flags.add_flag("--ub", action="store_true")
@@ -277,6 +278,7 @@ class Pokemon(commands.Cog):
     @flags.add_flag("--alolan", action="store_true")
     @flags.add_flag("--galarian", action="store_true")
     @flags.add_flag("--hisuian", action="store_true")
+    @flags.add_flag("--paradox", action="store_true")
     @flags.add_flag("--mythical", action="store_true")
     @flags.add_flag("--legendary", action="store_true")
     @flags.add_flag("--ub", action="store_true")
@@ -364,6 +366,7 @@ class Pokemon(commands.Cog):
     @flags.add_flag("--alolan", action="store_true")
     @flags.add_flag("--galarian", action="store_true")
     @flags.add_flag("--hisuian", action="store_true")
+    @flags.add_flag("--paradox", action="store_true")
     @flags.add_flag("--mythical", action="store_true")
     @flags.add_flag("--legendary", action="store_true")
     @flags.add_flag("--ub", action="store_true")
@@ -587,7 +590,7 @@ class Pokemon(commands.Cog):
             aggregations.append({"$match": {"auction_data.bidder_id": ctx.author.id}})
 
         rarity = []
-        for x in ("mythical", "legendary", "ub"):
+        for x in ("mythical", "legendary", "ub", "paradox"):
             if x in flags and flags[x]:
                 rarity += getattr(self.bot.data, f"list_{x}")
         if rarity:
@@ -779,6 +782,7 @@ class Pokemon(commands.Cog):
     @flags.add_flag("--alolan", action="store_true")
     @flags.add_flag("--galarian", action="store_true")
     @flags.add_flag("--hisuian", action="store_true")
+    @flags.add_flag("--paradox", action="store_true")
     @flags.add_flag("--mythical", action="store_true")
     @flags.add_flag("--legendary", action="store_true")
     @flags.add_flag("--ub", action="store_true")
@@ -884,6 +888,7 @@ class Pokemon(commands.Cog):
     @flags.add_flag("--alolan", action="store_true")
     @flags.add_flag("--galarian", action="store_true")
     @flags.add_flag("--hisuian", action="store_true")
+    @flags.add_flag("--paradox", action="store_true")
     @flags.add_flag("--mythical", action="store_true")
     @flags.add_flag("--legendary", action="store_true")
     @flags.add_flag("--ub", action="store_true")
